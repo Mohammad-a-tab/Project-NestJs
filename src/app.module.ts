98508e1274810a03b969b8fd92bb06f15a445a52
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-04-19 00:15:21 +0430

    make changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController, AuthController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'my-nestjs-db',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+    AuthModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
