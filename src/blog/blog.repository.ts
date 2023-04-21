7d94cc12d64ae887aab63f4ac4e49cb6889aea9d
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-04-22 03:16:13 +0430

    Implementation of the blog section

--- src/blog/blog.repository.ts
+++ src/blog/blog.repository.ts
@@ -1,23 +1,27 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { ForbiddenException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { DataSource, Repository } from 'typeorm';
 import { Blog } from './blog.entity';
 import { CreateBlogDTO } from './dto/create-blog.dto';
 import { User } from 'src/auth/user.entity';
-import { UserRepository } from "src/auth/auth.repository";
 
 @Injectable()
 export class BlogRepository extends Repository<Blog> {
-    constructor(
-            private dataSource: DataSource,
-            private readonly userRepository: UserRepository
-        ) {
+    constructor(private dataSource: DataSource) {
         super(Blog, dataSource.createEntityManager());
     }
     async createBlog(createBlogDto: CreateBlogDTO, user: User): Promise<Blog> {
-        const userSave = await this.userRepository.findOneBy({name:  user.name})
-        if(!userSave) 
-            throw new InternalServerErrorException("Operation failed")
-        const { title, description, image } = createBlogDto;
-        const 
+        let userBlogCount = await (await this.find({ where: { user } })).length
+        if (userBlogCount < 10) {
+            let { title, description, image } = createBlogDto;
+            const blog = this.create({ title, description, image, user });
+            let response = await this.save(blog).then(user => {
+                return user
+            }).catch(error => {
+                throw new InternalServerErrorException()
+            })
+            return response
+        } else {
+            throw new ForbiddenException("Each user can not save more than 10 blogs")
+        }
     }
 }
