c8f6320625209a0c3de6461c8ee02309f871c565
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-04-21 15:06:01 +0430

    Implementation of the blog section

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogService } from './blog.service';
+import { BlogController } from './blog.controller';
+import { BlogRepository } from './blog.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
-@Module({})
+@Module({
+    imports : [TypeOrmModule.forFeature([BlogRepository]), AuthModule],
+    controllers : [BlogController],
+    providers : [BlogService],
+  })
 export class BlogModule {}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { BlogRepository } from './blog.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateBlogDTO } from './dto/create-blog.dto';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class BlogService {
     constructor(
         @InjectRepository(BlogRepository) private readonly blogRepository: BlogRepository
     ) {}
+    async createBlog(createBlogDto: CreateBlogDTO, user: User): Promise<CreateBlogDTO>{
+        const mmd = await this.blogRepository.createBlog(createBlogDto, user);
+        return mmd
+    }
 }
