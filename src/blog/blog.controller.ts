e6fad5a05dc2088e96e61b6a62fc2e8a2739e058
mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-05-12 14:31:13 +0430

    fix ApiParam in swagger for nestjs project

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Param, Get, Post, Req, Delete, UseGuards, Patch } from '@nestjs/common';
-import { ApiTags, ApiResponse, ApiBearerAuth, ApiSecurity } from '@nestjs/swagger';
+import { ApiTags, ApiResponse, ApiBearerAuth, ApiSecurity, ApiConsumes, ApiBody, ApiParam } from '@nestjs/swagger';
 import { BlogService } from './blog.service';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateBlogDTO } from './dto/create-blog.dto';
@@ -19,6 +19,19 @@ export class BlogController {
     
     @Post('add')
     @UseGuards(AuthGuard('jwt'))
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+        description: 'create blog document',
+        schema: {
+            type: 'object',
+            properties: {
+                title : { type: 'string' },
+                description : { type: 'string' },
+                image : { type: 'file' }
+            },
+            required: ['name', 'email', 'password'],
+        },
+    })
     public createBlog(@Body() createBlogDto: CreateBlogDTO, @Req() req): Promise<Blog> {
         const user = req.user;
         return this.blogService.createBlog(createBlogDto, user);
@@ -30,16 +43,46 @@ export class BlogController {
         return blogs
     }
     @Get('getOne/:id')
+    @ApiParam({
+        name: 'id',
+        description: 'Blog ID',
+        type: 'string',
+        format: 'uuid',
+    })
     public getBlogById(@Param() blogIdDto: BlogIdDTO): Promise<Blog> {
         const { id } = blogIdDto
         return this.blogService.getBlogById(id);
     }
     @Delete('remove/:id')
+    @ApiParam({
+        name: 'id',
+        description: 'Blog ID',
+        type: 'string',
+        format: 'uuid',
+    })
     public deleteBlogById(@Param() blogIdDto: BlogIdDTO): Promise<object> {
         const { id } = blogIdDto;
         return this.blogService.deleteBlogById(id);
     }
     @Patch('edit/:id')
+    @ApiConsumes('multipart/form-data')
+    @ApiParam({
+        name: 'id',
+        description: 'Blog ID',
+        type: 'string',
+        format: 'uuid',
+    })
+    @ApiBody({
+        description: 'Update blog document By ID',
+        schema: {
+            type: 'object',
+            properties: {
+                title : { type: 'string' },
+                description : { type: 'string' },
+                image : { type: 'file' }
+            },
+        },
+    })
     public updateBlogById(@Param() blogId: BlogIdDTO, @Body() updateBlogDto: UpdateBlogDTO, @Req() req): Promise<object> {
         const user = req.user;
         const { id } = blogId;
